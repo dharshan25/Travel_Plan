1387f97216782bb97e5470a777fe84d4e5001a27
Z User <z@container>
2026-06-30 23:16:46 +0000

    19ab1012-9799-4662-a58e-f4af1069d877

--- src/lib/db.ts
+++ src/lib/db.ts
@@ -4,8 +4,31 @@ const globalForPrisma = globalThis as unknown as {
   prisma: PrismaClient | undefined
 }
 
+function createDb(): PrismaClient {
+  const tursoUrl = process.env.TURSO_DATABASE_URL;
+
+  if (tursoUrl) {
+    // Use Turso libSQL adapter for cloud deployment
+    // eslint-disable-next-line @typescript-eslint/no-require-imports
+    const { PrismaLibSQL } = require('@prisma/adapter-libsql');
+    // eslint-disable-next-line @typescript-eslint/no-require-imports
+    const { createClient } = require('@libsql/client');
+
+    const client = createClient({
+      url: tursoUrl,
+      authToken: process.env.TURSO_AUTH_TOKEN,
+    });
+
+    const adapter = new PrismaLibSQL(client);
+    return new PrismaClient({ adapter });
+  }
+
+  // Local development: use SQLite file
+  return new PrismaClient();
+}
+
 export const db =
   globalForPrisma.prisma ??
-  new PrismaClient()
+  createDb()
 
 if (process.env.NODE_ENV !== 'production') globalForPrisma.prisma = db
